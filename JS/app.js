193474d6b70c5fca241b45a7c787106f50c1c0d3
Riiskager <emil.axel.riis@gmail.com>
2025-08-19 08:30:46 +0200

    iygg

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,11 @@
-document.body.innerHTML += "Hello JavaScript!";
+//document.body.innerHTML += "Hello JavaScript!";
 //alert("Hello Dat.js");
 console.log("hello dat.js")
+
+
+
+function sayHello(){
+    document.querySelector("h3").textContent = "Hej " + document.querySelector("input").value;
+}
+
+document.querySelector("button").addEventListener("click", sayHello)
